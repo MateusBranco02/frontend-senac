8a0b485e131cafff704425e6936af79c83b11744
Mateus Silva Branco <143047033+MateusBranco02@users.noreply.github.com>
2025-09-03 15:55:02 -0300

    Adicionado interação ao botão de "Ver Detalhes"

---
+++ site-carros/src/js/script.js
@@ -0,0 +1,20 @@
+function Carro_1() {
+    const botaoCarro1 = document.getElementById('detalhes-carro-1');
+
+    botaoCarro1.innerText = 'Contato Feito!';
+    botaoCarro1.disabled = true;
+}
+
+function Carro_2() {
+    const botaoCarro2 = document.getElementById('detalhes-carro-2');
+
+    botaoCarro2.innerText = 'Contato Feito!';
+    botaoCarro2.disabled = true;
+}
+
+function Carro_3() {
+    const botaoCarro3 = document.getElementById('detalhes-carro-3');
+
+    botaoCarro3.innerText = 'Contato Feito!';
+    botaoCarro3.disabled = true;
+}
